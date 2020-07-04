d9d1a7a023c117bcff589ad9cc4a8cda362346be
Brandon Whisnant <galvatron@Galvatron.local>
2020-07-04 01:01:43 -0400

    added tailwind css and styled app

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import axios from 'axios'
+import '../styles/main.css'
 
 export default class Weather extends Component {
   constructor(props) {
@@ -58,29 +59,39 @@ export default class Weather extends Component {
 
   render() {
     return (
-      <div className="weather-container">
+      <div className='weather-container'>
         <form onSubmit={this.getWeatherInfo}>
           <input
+            class='bg-white text-center focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 my-6 block w-full appearance-none leading-normal'
             type='text'
             placeholder='Enter value'
             value={this.state.query}
             onChange={this.handleChange}
-          />
-          <button type='submit'>Submit</button>
+          ></input>
+          <button
+            class='bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 my-4 rounded'
+            type='submit'
+          >
+            Submit
+          </button>
         </form>
-        <div className='weather-container'>
+        <div className='weather-container rounded overflow-hidden shadow-lg px-10 py-10'>
           {this.state.submitted ? (
             <div>
               <p>
                 {' '}
-                In {this.state.name} the weather is described as {this.state.description}. The current temperature is {this.state.currentTemp} degrees.
+                In {this.state.name} the weather is described as{' '}
+                {this.state.description}. The current temperature is{' '}
+                {this.state.currentTemp} degrees.
               </p>
               <p>
-                The max today was {this.state.maxTemp} and the min was {this.state.minTemp}. It feels like it is {this.state.feelsLike} degrees.
+                The max today was {this.state.maxTemp} and the min was{' '}
+                {this.state.minTemp}. It feels like it is {this.state.feelsLike}{' '}
+                degrees.
               </p>
             </div>
           ) : (
-            <p>Search for weather</p>
+            <p>Search for weather...</p>
           )}
         </div>
       </div>
